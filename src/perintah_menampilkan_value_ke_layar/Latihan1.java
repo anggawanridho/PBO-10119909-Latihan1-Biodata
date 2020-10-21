1e9b03f7f48623bace461a31f84c034174b428ea
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-21 10:51:59 +0700

    Add files via upload
    first commit

---
+++ src/perintah_menampilkan_value_ke_layar/Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perintah_menampilkan_value_ke_layar;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" +"------------------" );
+        System.out.println("NAMA\t\t\t\t: Muhammad Anggawan Ridho Islami");
+        System.out.println("ALAMAT\t\t\t\t: Jl. Sekeloa Utara");
+        System.out.println("TEMPAT LLAHIR\t\t: Bandung");
+        System.out.println("TANGGAL LAHIRT\t\t: 20 Juni");
+        System.out.println("------------------");
+        System.out.println("HOBI\t\t\t\t: Calisthenic dan Basket");
+        System.out.println("MAKES\t\t\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t\t\t: Teh");
+                
+    }
+    
+}
